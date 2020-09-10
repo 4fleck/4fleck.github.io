7491e0381181f6e9bf93c63c3615c6b8d5ce38a2
4fleck <59694904+4fleck@users.noreply.github.com>
2020-09-10 08:44:14 +0300

    Add files via upload

---
+++ Page2/Page2.js
@@ -0,0 +1,4 @@
+function Back(){
+        alert("bye!")
+  
+}
---
+++ app.js
@@ -0,0 +1,6 @@
+
+function Button1(){
+document.getElementById("a")
+ location.herf="https://github.com/4fleck"
+}
+console.log("reeeeeeee")
